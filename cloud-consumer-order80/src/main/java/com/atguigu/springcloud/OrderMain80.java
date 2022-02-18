2f3a734ef0b7560bbd1fa5dbcebaa3a168625a77
sky <917968474@qq.com>
2022-02-18 21:12:38 +0800

    订单微服务模块 入驻eureka

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -3,11 +3,13 @@ package com.atguigu.springcloud;
 import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Sky
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderMain80.class, args);
