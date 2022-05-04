2d74e3238cc2aa6856e3f29627be95cbc9289587
sky <917968474@qq.com>
2022-05-04 21:29:59 +0800

    8001服务端  使用Hystrix 做 服务降级处理(使用注解)

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,6 +1,9 @@
 package com.atguigu.springcloud.service;
 
+import com.alibaba.druid.sql.visitor.ExportParameterizedOutputVisitor;
 import com.mysql.jdbc.TimeUtil;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,6 +15,7 @@ public class PaymentService {
 
 	/**
 	 * 正常访问
+	 *
 	 * @param id
 	 * @return
 	 */
@@ -20,14 +24,19 @@ public class PaymentService {
 		return "线程池" + Thread.currentThread().getName() + "paymentinfo_ok,id:" + id;
 	}
 
+	@HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler", commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")})
 	public String paymentInfo_Timeout(Integer id) {
 		try {
 			Thread.sleep(3000);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
-
 		}
+
 		return "线程池" + Thread.currentThread().getName() + "paymentinfo_timout,id:" + id;
+	}
+
+	public String paymentInfo_TimeoutHandler(Integer id) {
+
+		return "paymentInfo_TimeoutHandler," + id + "超时了";
 	}
 }
